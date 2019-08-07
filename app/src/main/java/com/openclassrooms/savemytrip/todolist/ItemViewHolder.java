42bad87de7d240efe518611340fd584d91538ad5
El Amri Omar <Omar_El_Amri@protonmail.com>
2019-08-07 17:08:43 +0100

    Initial commit

---
+++ app/src/main/java/com/openclassrooms/savemytrip/todolist/ItemViewHolder.java
@@ -0,0 +1,88 @@
+package com.openclassrooms.savemytrip.todolist;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Paint;
+import android.net.Uri;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+import com.openclassrooms.savemytrip.R;
+import com.openclassrooms.savemytrip.models.Item;
+
+import java.io.InputStream;
+import java.lang.ref.WeakReference;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class ItemViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+    @BindView(R.id.activity_todo_list_item_text) TextView textView;
+    @BindView(R.id.activity_todo_list_item_image) ImageView imageView;
+    @BindView(R.id.activity_todo_list_item_image_custom) ImageView imageViewCustom;
+    @BindView(R.id.activity_todo_list_item_remove) ImageButton imageButton;
+    @BindView(R.id.activity_todo_list_item_share) ImageButton imageButtonShare;
+
+    // FOR DATA
+    private WeakReference<ItemAdapter.Listener> callbackWeakRef;
+
+    public ItemViewHolder(View itemView) {
+        super(itemView);
+        ButterKnife.bind(this, itemView);
+    }
+
+    public void updateWithItem(Item item, ItemAdapter.Listener callback){
+        this.callbackWeakRef = new WeakReference<ItemAdapter.Listener>(callback);
+        this.textView.setText(item.getText());
+        this.imageButton.setOnClickListener(this);
+        this.imageButtonShare.setOnClickListener(this);
+        switch (item.getCategory()){
+            case 0: // TO VISIT
+                this.imageView.setBackgroundResource(R.drawable.ic_room_black_24px);
+                break;
+            case 1: // IDEAS
+                this.imageView.setBackgroundResource(R.drawable.ic_lightbulb_outline_black_24px);
+                break;
+            case 2: // RESTAURANTS
+                this.imageView.setBackgroundResource(R.drawable.ic_local_cafe_black_24px);
+                break;
+        }
+        if (item.getSelected()){
+            textView.setPaintFlags(textView.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
+        } else {
+            textView.setPaintFlags(textView.getPaintFlags() & (~Paint.STRIKE_THRU_TEXT_FLAG));
+        }
+
+        if(item.getImageUri() != null){
+            Uri mUri = Uri.parse(item.getImageUri());
+
+            Log.i("TEST Ok : ", mUri.toString());
+            this.imageViewCustom.setImageURI(mUri);
+        }else{
+            this.imageViewCustom.setVisibility(View.GONE);
+            this.imageButtonShare.setVisibility(View.GONE);
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        ItemAdapter.Listener callback = callbackWeakRef.get();
+        if (callback != null){
+            if (view.getId() == R.id.activity_todo_list_item_remove) {
+                callback.onClickDeleteButton(getAdapterPosition());
+            }
+
+            if (view.getId() == R.id.activity_todo_list_item_share) {
+                callback.onClickShareButton(getAdapterPosition());
+            }
+        }
+    }
+
+}
---
+++ app/src/main/java/com/openclassrooms/savemytrip/todolist/TodoListActivity.java
@@ -0,0 +1,213 @@
+package com.openclassrooms.savemytrip.todolist;
+
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModelProviders;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+import com.openclassrooms.savemytrip.R;
+import com.openclassrooms.savemytrip.base.BaseActivity;
+import com.openclassrooms.savemytrip.injection.Injection;
+import com.openclassrooms.savemytrip.injection.ViewModelFactory;
+import com.openclassrooms.savemytrip.models.Item;
+import com.openclassrooms.savemytrip.models.User;
+import com.openclassrooms.savemytrip.utils.ItemClickSupport;
+import com.openclassrooms.savemytrip.utils.StorageUtils;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.OnClick;
+import pub.devrel.easypermissions.EasyPermissions;
+
+import static android.content.Intent.FLAG_GRANT_PERSISTABLE_URI_PERMISSION;
+import static android.content.Intent.FLAG_GRANT_READ_URI_PERMISSION;
+
+public class TodoListActivity extends BaseActivity implements ItemAdapter.Listener {
+
+    private static final String TAG = "TodoListActivity";
+
+    // FOR DESIGN
+    @BindView(R.id.todo_list_activity_recycler_view) RecyclerView recyclerView;
+    @BindView(R.id.todo_list_activity_spinner) Spinner spinner;
+    @BindView(R.id.todo_list_activity_edit_text) EditText editText;
+    @BindView(R.id.todo_list_activity_header_profile_image) ImageView profileImage;
+    @BindView(R.id.todo_list_activity_header_profile_text) TextView profileText;
+    @BindView(R.id.todo_list_activity_button_add_img) ImageButton buttonAddImg;
+
+    // FOR DATA
+    private ItemViewModel itemViewModel;
+    private ItemAdapter adapter;
+    private static int USER_ID = 1;
+    private static int RESULT_LOAD_IMAGE = 1;
+
+    private Uri imageUri;
+
+    @Override
+    public int getLayoutContentViewID() { return R.layout.activity_todo_list; }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.configureToolbar();
+        this.configureSpinner();
+
+        this.configureRecyclerView();
+        this.configureViewModel();
+
+        this.getCurrentUser(USER_ID);
+        this.getItems(USER_ID);
+    }
+
+    // -------------------
+    // ACTIONS
+    // -------------------
+
+    @OnClick(R.id.todo_list_activity_button_add_img)
+    public void onClickAddimgButton() {
+        //Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+        Intent photoPickerIntent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+
+        photoPickerIntent.addFlags(FLAG_GRANT_READ_URI_PERMISSION);
+        photoPickerIntent.addFlags(FLAG_GRANT_PERSISTABLE_URI_PERMISSION);
+
+        photoPickerIntent.setType("image/*");
+        startActivityForResult(photoPickerIntent, RESULT_LOAD_IMAGE);
+    }
+
+    @Override
+    protected void onActivityResult(int reqCode, int resultCode, Intent data) {
+        super.onActivityResult(reqCode, resultCode, data);
+
+        if (resultCode == RESULT_OK) {
+            try {
+                this.imageUri = data.getData();
+                final InputStream imageStream = getContentResolver().openInputStream(imageUri);
+                Bitmap selectedImage = BitmapFactory.decodeStream(imageStream);
+
+                BitmapDrawable bitmapDrawable = new BitmapDrawable(getResources(), selectedImage);
+                buttonAddImg.setBackground(bitmapDrawable);
+
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+                Toast.makeText(this, "Something went wrong", Toast.LENGTH_LONG).show();
+            }
+
+        }else {
+            Toast.makeText(this, "You haven't picked Image",Toast.LENGTH_LONG).show();
+        }
+    }
+
+
+    @OnClick(R.id.todo_list_activity_button_add)
+    public void onClickAddButton() { this.createItem(); }
+
+    @Override
+    public void onClickDeleteButton(int position) {
+        Log.i(TAG, "DELETE");
+        this.deleteItem(this.adapter.getItem(position));
+    }
+
+    @Override
+    public void onClickShareButton(int position) {
+        Log.i(TAG, "SHARE");
+
+        Item item = this.adapter.getItem(position);
+        Uri mUri = Uri.parse(item.getImageUri());
+
+        Intent sharingIntent = new Intent(Intent.ACTION_SEND);
+        sharingIntent.setType("image/*");
+        sharingIntent.putExtra(Intent.EXTRA_STREAM, mUri);
+        startActivity(Intent.createChooser(sharingIntent, "Share Image"));
+    }
+
+
+    // -------------------
+    // DATA
+    // -------------------
+
+    private void configureViewModel(){
+        ViewModelFactory mViewModelFactory = Injection.provideViewModelFactory(this);
+        this.itemViewModel = ViewModelProviders.of(this, mViewModelFactory).get(ItemViewModel.class);
+        this.itemViewModel.init(USER_ID);
+    }
+
+
+    private void getCurrentUser(int userId){
+        this.itemViewModel.getUser(userId).observe(this, this::updateHeader);
+    }
+
+    // ---
+
+    private void getItems(int userId){
+        this.itemViewModel.getItems(userId).observe(this, this::updateItemsList);
+    }
+
+    private void createItem(){
+        Item item = new Item(this.editText.getText().toString(), this.spinner.getSelectedItemPosition(), USER_ID, this.imageUri);
+        this.editText.setText("");
+        this.itemViewModel.createItem(item);
+        this.buttonAddImg.setBackgroundResource(R.drawable.baseline_photo_size_select_actual_black_24);
+    }
+
+    private void deleteItem(Item item){
+        this.itemViewModel.deleteItem(item.getId());
+    }
+
+    private void updateItem(Item item){
+        item.setSelected(!item.getSelected());
+        this.itemViewModel.updateItem(item);
+    }
+
+    // -------------------
+    // UI
+    // -------------------
+
+    private void configureSpinner(){
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.category_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+    }
+
+    private void configureRecyclerView(){
+        this.adapter = new ItemAdapter(this);
+        this.recyclerView.setAdapter(this.adapter);
+        this.recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        ItemClickSupport.addTo(recyclerView, R.layout.activity_todo_list_item)
+                .setOnItemClickListener((recyclerView1, position, v) -> this.updateItem(this.adapter.getItem(position)));
+    }
+
+    private void updateHeader(User user){
+        this.profileText.setText(user.getUsername());
+        Glide.with(this).load(user.getUrlPicture()).apply(RequestOptions.circleCropTransform()).into(this.profileImage);
+    }
+
+    private void updateItemsList(List<Item> items){
+        this.adapter.updateData(items);
+    }
+
+}
